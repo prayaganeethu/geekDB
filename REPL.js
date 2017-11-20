7d6c739360d43ba14197f3d96162a92ce655b166
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-20 15:42:08 +0530

    Added lib folder. Added a module keyValueStore.js in it, which splits the input based on spaces and prints it

--- REPL.js
+++ REPL.js
@@ -1,11 +1,15 @@
 const prompt = require('prompt')
+const {execute} = require('./lib/keyValueStore')
 
 prompt.start()
 
 function recursivePrompt () {
   prompt.get(['command'], function (err, response) {
     if (err) throw err
-    if (response.command) console.log(response.command + ' ' + Date())
+    if (response.command) {
+      console.log(response.command + ' ' + Date())
+      execute(response.command)
+    }
     recursivePrompt()
   })
 }
