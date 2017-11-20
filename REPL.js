d6881f250641b510a1758a20f9c9ae8f34694526
Neethu Mohandas <prayaganeethu@gmail.com>
2017-11-20 17:22:54 +0530

    Used 'readline' prompt instead, in REPL. Exits gracefully on CTRL+C

--- REPL.js
+++ REPL.js
@@ -1,17 +1,20 @@
-const prompt = require('prompt')
+const readline = require('readline')
 const {execute} = require('./lib/keyValueStore')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'kvs>> '
+})
 
-prompt.start()
+rl.prompt()
 
-function recursivePrompt () {
-  prompt.get(['command'], function (err, response) {
-    if (err) throw err
-    if (response.command) {
-      console.log(response.command + ' ' + Date())
-      execute(response.command)
-    }
-    recursivePrompt()
-  })
-}
-
-recursivePrompt()
+rl.on('line', (command) => {
+  if (command) {
+    console.log(command + ' ' + Date())
+    execute(command)
+  }
+  rl.prompt()
+}).on('close', () => {
+  console.log('Exiting...')
+  process.exit(0)
+})
